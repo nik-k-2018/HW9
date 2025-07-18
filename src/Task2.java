4a8297df69b36e127f71998d98506e07a08da405
Azerty <nik.k.2018@mail.ru>
2025-07-18 18:02:58 +0300

    Задача 2. вывод ФИО заглавными буквами

---
+++ src/Task2.java
@@ -0,0 +1,12 @@
+public class Task2 {
+    public static void main(String[] args) {
+        // Исходная строка с Ф.И.О.
+        String fullName = "Ivanov Ivan Ivanovich";
+
+        // Приводим строку к верхнему регистру
+        String fullNameUpperCase = fullName.toUpperCase();
+
+        // Выводим результат
+        System.out.println("Данные Ф. И. О. сотрудника для заполнения отчета — " + fullNameUpperCase);
+    }
+}
